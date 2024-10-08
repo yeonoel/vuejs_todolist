c38befd149bbd355c8bff003c7d6cfaceaaccfe6
yeonoel <yeopevroguinoel@gmail.com>
2024-10-08 22:23:02 +0000

    FIrst commit

---
+++ scripts.js
@@ -0,0 +1,36 @@
+
+
+
+const todoLite = {
+    data() {
+        return {
+            todos: [],
+            todosFishix: [],
+            isSelected: false,
+            newtodo: {
+                
+            },
+            newTodoFihish: {
+
+            }
+        }
+    },
+    methods: {
+        addnewToDo(){
+            if (this.newtodo.do) {
+                this.todos.push({do: this.newtodo.do, isDone: false});
+                this.newtodo = { } 
+            } else {
+                alert("Vous n'avez entré de tache")
+            }
+        },
+        addSelection(item){
+            item.isDone = !item.isDone;
+                
+        }
+    }
+    
+};
+
+ 
+Vue.createApp(todoLite).mount('#app');
